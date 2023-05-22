ec0e7efe9187c2d123d0b8fc87944a07c1f312df
Solène <81546747+solenedel@users.noreply.github.com>
2023-05-22 16:34:34 -0700

    work on practice template 1

--- pages/pulse/index.tsx
+++ pages/pulse/index.tsx
@@ -32,6 +32,39 @@ const pricingListItems = {
   ],
 };
 
+const FAQ = [
+  {
+    question: 'How does the 30-day free trial work?',
+    answer:
+      'You can use Pulse absolutely free for 30 days. After 30 days, we will charge the credit card you used when you sign up. If you choose to cancel your account before your trial ends, your card will not be charged.',
+  },
+  {
+    question: 'What is a “Financial Account”?',
+    answer:
+      'Financial Accounts are a grouping of income and expense items that can be used for tracking or reporting purposes. They’re designed to be flexible and can be used to differentiate between bank accounts, companies, individuals or between personal and business entries.',
+  },
+  {
+    question: 'Will I be able to cancel my account if I don’t use Pulse?',
+    answer:
+      'Yes – you can cancel your account at any time. To cancel your account, go to your billing page and click on “Please cancel my account” at the bottom of the page. The account owner is the only one who can cancel an account. An email request to delete an account is not considered cancellation.',
+  },
+  {
+    question: 'If I upgrade an existing account, do I get another free trial?',
+    answer:
+      'Brand new accounts are the only ones eligible for a 30-day free trial. If you upgrade an existing account, your credit card will be billed the new amount on the date of your next billing cycle.',
+  },
+  {
+    question: 'Do I have to commit to a long-term contract?',
+    answer:
+      'No. There is no contract when you use Pulse, other than agreeing to our Terms of Service.',
+  },
+  {
+    question: 'What types of payments do you accept?',
+    answer:
+      'We accept payments made online using Visa, Mastercard, Discover, and American Express',
+  },
+];
+
 function Pulse() {
   return (
     <div className="h-screen overflow-y-scroll bg-[--pulse-bg] w-screen border-2 border-black">
@@ -130,7 +163,17 @@ function Pulse() {
         </div>
       </main>
       {/* FAQ SECTION */}
-      <section className="bg-[--pulse-white] h-80"></section>
+      <section className="bg-[--pulse-white] h-fit py-14 px-16">
+        <h2 className="text-4xl text-black my-10">FAQ</h2>
+        <div className="grid grid-cols-2 gap-10 grid-flow-row">
+          {FAQ.map((q) => (
+            <div className="text-left flex flex-col" key={q.question}>
+              <h3 className="text-lg font-bold">{q.question}</h3>
+              <p>{q.answer}</p>
+            </div>
+          ))}
+        </div>
+      </section>
       {/* FOOTER */}
       <footer className="bg-[--pulse-black] h-80"></footer>
     </div>
